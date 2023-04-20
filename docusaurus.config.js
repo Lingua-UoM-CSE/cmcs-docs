0752fd7a4672cdba3eb72b2da206aaf4747b8a1b
indunilu <induniludayangana.18@cse.mrt.ac.lk>
2023-04-20 18:08:32 +0530

    refactor

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,65 +71,65 @@ const config = {
           alt: "Lingua-logo",
           src: "img/122144686.png",
         },
-        items: [
-          {
-            to: "/docs",
-            type: "docSidebar",
-            sidebarId: "tutorialSidebar",
-            position: "left",
-            label: "Docs",
-          },
-          { to: "/blog", label: "Blog", position: "left" },
-          {
-            href: "https://github.com/Lingua-UoM-CSE",
-            label: "GitHub",
-            position: "right",
-          },
-        ],
+        // items: [
+        //   {
+        //     to: "/docs",
+        //     type: "docSidebar",
+        //     sidebarId: "tutorialSidebar",
+        //     position: "left",
+        //     label: "Docs",
+        //   },
+        //   { to: "/blog", label: "Blog", position: "left" },
+        //   {
+        //     href: "https://github.com/Lingua-UoM-CSE",
+        //     label: "GitHub",
+        //     position: "right",
+        //   },
+        // ],
       },
       footer: {
         style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Docs",
-                to: "/docs",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
+        // links: [
+        //   {
+        //     title: "Docs",
+        //     items: [
+        //       {
+        //         label: "Docs",
+        //         to: "/docs",
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: "Community",
+        //     items: [
+        //       {
+        //         label: "Stack Overflow",
+        //         href: "https://stackoverflow.com/questions/tagged/docusaurus",
+        //       },
+        //       {
+        //         label: "Discord",
+        //         href: "https://discordapp.com/invite/docusaurus",
+        //       },
+        //       {
+        //         label: "Twitter",
+        //         href: "https://twitter.com/docusaurus",
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: "More",
+        //     items: [
+        //       {
+        //         label: "Blog",
+        //         to: "/blog",
+        //       },
+        //       {
+        //         label: "GitHub",
+        //         href: "https://github.com/facebook/docusaurus",
+        //       },
+        //     ],
+        //   },
+        // ],
         copyright: `Copyright © ${new Date().getFullYear()} Lingua UoM CSE.`,
       },
       prism: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,12 +15,12 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
+          {/* <Link
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
             Read docs↗️
-          </Link>
+          </Link> */}
         </div>
       </div>
     </header>
