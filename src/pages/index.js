c585103f500c50761582080862b4578990f02ddd
indunilu <induniludayangana.18@cse.mrt.ac.lk>
2023-04-20 19:21:00 +0530

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,9 +15,12 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          {/* <Link className="button button--secondary button--lg" to="/docs">
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
             Read docs↗️
-          </Link> */}
+          </Link>
         </div>
       </div>
     </header>
